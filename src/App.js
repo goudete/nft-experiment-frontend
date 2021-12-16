d5e99f71d00e2ba07e68c696c800d70dee3c13dc
Enrique Goudet <goudetenrique@gmail.com>
2021-12-15 22:41:53 -0600

    added some style and began work for uploading files

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 import { ethers } from 'ethers';
-import myEpicNft from './utils/myEpicNFT.json'
+import mintNFT from './utils/mintNFT.json';
 
 const TWITTER_HANDLE = '_eggr_';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = 'https://rinkeby.rarible.com/token/INSERT_CONTRACT_ADDRESS_HERE:INSERT_TOKEN_ID_HERE';
 const TOTAL_MINT_COUNT = 50;
-const CONTRACT_ADDRESS = "0xDE79D1366572059ca68f0fF164DDc115765c18d8";
+const CONTRACT_ADDRESS = "0x26C58614683F010Ca9383fB38a30d9AaF49FC105";
+const { NFT_STORAGE_KEY } = "./consts";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
@@ -19,10 +20,18 @@ const App = () => {
         if (!ethereum) {
             console.log('Make sure you have MetaMask!');
             return;
-        } else {
-            console.log("We have the ethereum object", ethereum);
         }
 
+        let chainId = await ethereum.request({ method: 'eth_chainId' });
+        console.log("Connected to chain " + chainId);
+        const rinkebyChainId = "0x4";
+
+        if (chainId !== rinkebyChainId) {
+            console.log("You are not connected to the Rinkeby Test Network!");
+            return;
+        }
+
+        console.log("We have the ethereum object", ethereum);
         const accounts = await ethereum.request({ method: 'eth_accounts' });
 
         if (accounts.length !== 0) {
@@ -49,6 +58,7 @@ const App = () => {
             setCurrentAccount(accounts[0]);
             
             setupEventListener();
+            return alert('Account connected!');
         } catch(err) {
             console.log(err);
 
@@ -56,17 +66,32 @@ const App = () => {
     };
 
     const askContractToMintNft = async () => {
-
         try {
             const { ethereum } = window;
 
             if (ethereum) {
                 const provider = new ethers.providers.Web3Provider(ethereum);
                 const signer = provider.getSigner();
-                const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+                const connectedSmartContract = new ethers.Contract(CONTRACT_ADDRESS, mintNFT.abi, signer);
 
-                console.log("Going to pop wallet now to pay gas...")
-                let nftTxn = await connectedContract.makeAnEpicNFT();
+
+                `===========`
+                const name = 'Saint Benedict';
+                const description = 'ora et labora'
+                // First we use the nft.storage client library to add the image and metadata to IPFS / Filecoin
+                const client = new NFTStorage({ token: NFT_STORAGE_KEY });
+                setStatus("Uploading to nft.storage...")
+                const metadata = await client.store({
+                    name,
+                    description,
+                    image,
+                });
+                setStatus(`Upload complete! Minting token with metadata URI: ${metadata.url}`);
+                
+                `===========`
+
+                console.log("Going to pop wallet now to pay gas...");
+                let nftTxn = await connectedSmartContract.mintNFT();
 
                 console.log("Mining...please wait.")
                 await nftTxn.wait();
@@ -87,11 +112,10 @@ const App = () => {
             if (ethereum) {
                 const provider = new ethers.providers.Web3Provider(ethereum);
                 const signer = provider.getSigner();
-                const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+                const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, mintNFT.abi, signer);
     
-                // Captures 'event' when our contract throws it.
-                connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
-                    console.log(from, tokenId.toNumber())
+                connectedContract.on("NewMint", (from, tokenId) => {
+                    console.log('divine calling:', from, tokenId.toNumber());
                     alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
                 });
         
@@ -111,9 +135,41 @@ const App = () => {
     );
 
     const renderMintContainer = () => (
-        <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
-            Mint NFT
-        </button>
+        <div>
+            <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+                mint non-fungible token
+            </button>
+            <div>
+                Drop an image file or click below to select.
+                {/* import Upload component from ant or find blueprint one */}
+                <Upload
+                    name="avatar"
+                    accept=".jpeg,.jpg,.png,.gif"
+                    listType="picture-card"
+                    className="avatar-uploader"
+                    showUploadList={false}
+                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    beforeUpload={beforeUpload}
+                >
+                {uploadButton}
+                </Upload>
+            </div>
+        </div>
+    );
+
+    const beforeUpload = (file, fileList) => {
+        console.log(file, fileList);
+        setFile(file);
+        setPreviewURL(URL.createObjectURL(file));
+        return false;
+    }
+
+    const uploadButton = () => (
+        <div>
+            <div style={{ marginTop: 8 }}>
+                Choose image
+            </div>
+        </div>
     );
 
     useEffect(() => {
@@ -124,10 +180,6 @@ const App = () => {
         <div className="App">
             <div className="container">
                 <div className="header-container">
-                    <p className="header gradient-text">My NFT Collection</p>
-                    <p className="sub-text">
-                        Each unique. Each beautiful. Discover your NFT today.
-                    </p>
                     {currentAccount === "" ? (
                         renderNotConnectedContainer()
                     ) : (
@@ -141,7 +193,7 @@ const App = () => {
                         href={TWITTER_LINK}
                         target="_blank"
                         rel="noreferrer"
-                    >{`built on @${TWITTER_HANDLE}`}</a>
+                    >{`built by @${TWITTER_HANDLE}`}</a>
                 </div>
             </div>
         </div>
